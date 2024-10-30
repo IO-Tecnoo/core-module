04dedb110849e2d244455dd05fad0d6b272e8005
Mostafa Sewidan <35866891+MostafaSewidan@users.noreply.github.com>
2024-10-30 21:04:37 +0300

    Create CoreServiceProvider.php

---
+++ CoreServiceProvider.php
@@ -0,0 +1,20 @@
+namespace Sewidan\Core;
+
+use Illuminate\Support\ServiceProvider;
+use Sewidan\Core\Console\UpdateCoreModule;
+
+class CoreServiceProvider extends ServiceProvider
+{
+    public function register()
+    {
+        // Register the command
+        $this->commands([
+            UpdateCoreModule::class,
+        ]);
+    }
+
+    public function boot()
+    {
+        // Bootstrapping logic here if needed
+    }
+}
